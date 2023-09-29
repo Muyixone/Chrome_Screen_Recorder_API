08a7e9fd548b043037e659ccd4a81e3aed5e2e09
Osamuyi <muyixone@yahoo.com>
2023-09-29 09:42:36 +0100

    Refractor the multer.diskStorage method

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 
+const fileUploadConfig = require('./config/uploadFile');
 const route = require('./routes/uploadVideo');
+
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3500;
 
-const storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './videos');
-  },
-  filename: function (req, file, callback) {
-    callback(null, Date.now() + '-' + file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, callback) {
+//     callback(null, './videos');
+//   },
+//   filename: function (req, file, callback) {
+//     callback(null, Date.now() + '-' + file.originalname);
+//   },
+// });
 
-const upload = multer({ storage: storage });
+const upload = multer(fileUploadConfig);
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/public/upload.html'));
---
+++ config/uploadFile.js
@@ -0,0 +1,52 @@
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+function fileFilter(req, file, callback) {
+  let errorMessage = '';
+  // upload only mp4 and mkv format
+
+  if (!file.originalname.match(/\.(mp4|MPEG-4|mkv)$/)) {
+    errorMessage = `Wrong file type ${file.originalname
+      .split('.')
+      .pop()} found. Only mp4 video files are allowed!`;
+    return callback(errorMessage);
+  }
+
+  callback(null, true);
+}
+
+function destinationPath(req, file, callback) {
+  let stat = null;
+  try {
+    stat = fs.statSync(process.env.FILE_UPLOAD_PATH);
+  } catch (error) {
+    fs.mkdirSync(process.env.FILE_UPLOAD_PATH);
+  }
+
+  callback(null, process.env.FILE_UPLOAD_PATH);
+}
+
+function uniqueFileName(req, file, callback) {
+  callback(
+    null,
+    file.fieldname + '_' + Date.now() + path.extname(file.originalname)
+  );
+}
+
+const limits = {
+  fileSize: process.env.FILE_SIZE * 1024 * 1024, // 100MB
+};
+
+const videoStorage = multer.diskStorage({
+  destination: destinationPath,
+  filename: uniqueFileName,
+});
+
+const fileUploadConfig = {
+  storage: videoStorage,
+  limits: limits,
+  fileFilter: fileFilter,
+};
+
+module.exports = fileUploadConfig;
