317922a951ce864010f1f5d43c8cbedfd38a21c8
Osamuyi <muyixone@yahoo.com>
2023-09-29 14:18:48 +0100

    Add getFile and readFileFromDb helper functions

--- dB/dBOperations.js
+++ dB/dBOperations.js
@@ -7,11 +7,39 @@ const saveToDb = (data) => {
   if (!currentData) {
     currentData = '{}';
   }
+  //Parse current Data to an Object
   const parsedData = JSON.parse(currentData);
 
+  // Update data in the object
   parsedData[data.id] = data;
+
+  //convert the modified object back to JSON
   currentData = JSON.stringify(parsedData);
+
   fs.writeFileSync(fileName, currentData, 'utf8');
+};
+
+const readFromDb = () => {
+  let currentData = fs.readFileSync(fileName, 'utf8');
+
+  if (!currentData) {
+    currentData = '{}';
+  }
+  const parsedData = JSON.parse(currentData);
+  return parsedData;
+};
+
+const getFile = (fileId) => {
+  if (!fileId) {
+    return null;
+  }
+  let currentData = fs.readFileSync(fileName, 'utf8');
+
+  if (!currentData) {
+    currentData = '{}';
+  }
+  const parsedData = JSON.parse(currentData);
+  return parsedData[fileId];
 };
 
 module.exports = {
