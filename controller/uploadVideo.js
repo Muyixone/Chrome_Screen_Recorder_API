fabc90bc2b5062f255bcdd984a744ae0f647386e
Osamuyi <muyixone@yahoo.com>
2023-09-29 13:58:38 +0100

    create uploadVideo controller

--- controller/uploadVideo.js
+++ controller/uploadVideo.js
@@ -1,5 +1,29 @@
+const path = require('path');
+const { saveToDb } = require('../dB/dBOperations');
+
 const uploadVideo = (req, res) => {
-  return console.log(`Video uploaded: ${req.file.filename}`);
+  const fileIdWithExtension = req.file.filename.split('_')[1];
+
+  // remove the extension name from the fileId
+  const fileId = path.parse(fileIdWithExtension).name;
+
+  const link = `http://${req.hostname}:${process.env.PORT}/video/${fileId}`;
+
+  res.json({
+    success: true,
+    link: link,
+  });
+
+  const attributesToSave = {
+    id: fileId,
+    name: req.file.originalname,
+    size: req.file.size,
+    path: req.file.path,
+    encoding: req.file.encoding,
+    details: req.body.details ? req.body.details : '',
+  };
+
+  return saveToDb(attributesToSave);
 };
 
 module.exports = uploadVideo;
