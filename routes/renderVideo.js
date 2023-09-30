6c4e235553ab370d3cf117630c23914def277c4d
Osamuyi <muyixone@yahoo.com>
2023-09-30 02:00:58 +0100

    Create the renderVideo route

--- routes/renderVideo.js
+++ routes/renderVideo.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const renderVideo = require('../controller/renderVideo');
+const sendAllVideos = require('../controller/renderVideo');
 const streamVideo = require('../controller/streamVideo');
 
 const router = express.Router();
 
-// router.get('/', renderVideo);
+router.get('/', sendAllVideos);
 router.get('/:videoName', streamVideo);
 
 module.exports = router;
