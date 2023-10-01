c3c39a239d5509f1c068590716b866b7dc4de5a5
Osamuyi <muyixone@yahoo.com>
2023-10-01 13:57:45 +0100

    Create the starteRecording controller

---
+++ controller/startRecording.js
@@ -0,0 +1,21 @@
+const videoModel = require('../models/videoInfo');
+
+const startRecording = async (req, res) => {
+  const { mimeType } = req.body;
+  if (!mimeType) {
+    return console.log('Please send a mimeType for your file');
+  }
+
+  const data = new videoModel({
+    mimeType,
+  });
+
+  await data.save();
+
+  return res.json({
+    videoId: data._id,
+    status: 200,
+  });
+};
+
+module.exports = startRecording;
