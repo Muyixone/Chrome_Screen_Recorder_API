f9d8698df5bee7293925cdad004d7853224dfaa3
Osamuyi <muyixone@yahoo.com>
2023-10-01 18:09:33 +0100

    Remove mimetype field

--- controller/startRecording.js
+++ controller/startRecording.js
@@ -1,16 +1,18 @@
 const videoModel = require('../models/videoInfo');
 
 const startRecording = async (req, res) => {
-  const { mimeType } = req.body;
-  if (!mimeType) {
-    return console.log('Please send a mimeType for your file');
-  }
+  // const { mimeType } = req.body;
+  // if (!mimeType) {
+  //   return console.log('Please send a mimeType for your file');
+  // }
 
   const data = new videoModel({
-    mimeType,
+    ...req.body,
   });
 
   await data.save();
+
+  console.log(data);
 
   return res.json({
     videoId: data._id,
