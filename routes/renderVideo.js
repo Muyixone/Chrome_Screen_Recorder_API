ec217797b33825570e4ae3ff9c10bc522e9b5695
Osamuyi <muyixone@yahoo.com>
2023-10-02 20:09:37 +0100

    fixed a bug: placed the stream video route ahead of the get video

--- routes/renderVideo.js
+++ routes/renderVideo.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const sendAllVideos = require('../controller/renderVideo');
+const getVideo = require('../controller/renderVideo');
 const streamVideo = require('../controller/streamVideo');
 
 const router = express.Router();
 
-router.get('/', sendAllVideos);
-router.get('/:videoName', streamVideo);
+router.get('/play/:videoId', streamVideo);
+router.get('/:id', getVideo);
 
 module.exports = router;
