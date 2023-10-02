0c0bb9e446fb62f68a3e22d4fcda9526a6c0d5b1
Osamuyi <muyixone@yahoo.com>
2023-10-02 23:31:40 +0100

    Wrap the startRecording function in a try/catch block

--- controller/startRecording.js
+++ controller/startRecording.js
@@ -1,23 +1,22 @@
 const videoModel = require('../models/videoInfo');
 
 const startRecording = async (req, res) => {
-  // const { mimeType } = req.body;
-  // if (!mimeType) {
-  //   return console.log('Please send a mimeType for your file');
-  // }
-
   const data = new videoModel({
     ...req.body,
   });
 
-  await data.save();
+  try {
+    await data.save();
 
-  console.log(data);
-
-  return res.json({
-    videoId: data._id,
-    status: 200,
-  });
+    return res.json({
+      videoId: data._id,
+      status: 200,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Something went wrong',
+    });
+  }
 };
 
 module.exports = startRecording;
