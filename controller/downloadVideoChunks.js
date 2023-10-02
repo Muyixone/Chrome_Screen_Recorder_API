2afed624878a221792aa0a251c052574231244c0
Osamuyi <muyixone@yahoo.com>
2023-10-02 23:23:10 +0100

    Impletement the delete buffer file from server mememory once the decoding to video format is done

--- controller/downloadVideoChunks.js
+++ controller/downloadVideoChunks.js
@@ -2,46 +2,52 @@ const mongoose = require('mongoose');
 const videoModel = require('../models/videoInfo');
 const fs = require('fs');
 const path = require('path');
-const { saveToDb, getFileById } = require('../dB/dBOperations');
+const { saveToDb, getFileById, deleteFile } = require('../dB/dBOperations');
 
 const downloadVideo = async (req, res) => {
   const videoId = req.params.id;
-  const mimeType = req.headers['content-type'];
-  console.log(mimeType);
+  // const mimeType = req.headers['content-type'];
 
   if (!mongoose.Types.ObjectId.isValid(videoId)) {
     return console.log('Invalid Id');
   }
 
-  const video = await videoModel.findById({ _id: videoId });
-  if (!video) {
-    return console.log('File not found');
+  try {
+    const video = await videoModel.findById({ _id: videoId });
+    if (!video) {
+      return console.log('File not found');
+    }
+
+    // Set the filetype in the data base to a specific video format
+    video.set('mimeType', 'video/webm');
+    await video.save();
+
+    // Get chunks of data from client
+    const chunks = [];
+    req.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
+
+    req.on('end', async () => {
+      // Combine all received chunks into a single buffer array
+      const dataBuffer = Buffer.concat(chunks);
+
+      // Save to a local file in the server
+      await saveToDb(dataBuffer, videoId);
+    });
+    return res.json({
+      message: `Chunks of data for videoId ${video._id} recieved`,
+    });
+  } catch (error) {
+    res.json({
+      message: 'Something went wrong',
+    });
   }
-
-  // Set the filetype in the data base to a specific video format
-  video.set('mimeType', 'video/webm');
-  await video.save();
-
-  const chunks = [];
-  req.on('data', (chunk) => {
-    chunks.push(chunk);
-  });
-
-  req.on('end', async () => {
-    // Combine all received chunks into a single buffer
-    const dataBuffer = Buffer.concat(chunks);
-
-    await saveToDb(dataBuffer, videoId);
-  });
-  return res.json({
-    message: 'Chunks of data recieved',
-  });
 };
 
 const finalVideoChunk = async (req, res) => {
   try {
     const videoId = req.params.id;
-    // const mimeType = req.headers['content-type'];
 
     if (!mongoose.Types.ObjectId.isValid(videoId)) {
       return console.log('Invalid Id');
@@ -49,7 +55,9 @@ const finalVideoChunk = async (req, res) => {
 
     const videoData = await videoModel.findById({ _id: videoId });
     if (!videoData) {
-      return console.log('File not found');
+      return res.status(404).json({
+        message: 'Video file missing',
+      });
     }
 
     const chunks = [];
@@ -83,6 +91,9 @@ const finalVideoChunk = async (req, res) => {
           });
           await videoData.save();
 
+          //Once buffer ias been decoded back to the orinal video file, delete the buffer file in mememory
+          await deleteFile(videoId);
+
           return res.json({
             message: 'Video saved',
             status: 200,
@@ -96,7 +107,7 @@ const finalVideoChunk = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({
-      message: 'Something went wrong',
+      message: error,
     });
   }
 };
