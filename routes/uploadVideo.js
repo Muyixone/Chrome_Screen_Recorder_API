01e200985cc773d9fc2b0351e8dd07c4008a19d9
Osamuyi <muyixone@yahoo.com>
2023-10-02 23:32:50 +0100

    Refractor the uploadVideo route

--- routes/uploadVideo.js
+++ routes/uploadVideo.js
@@ -5,14 +5,8 @@ const {
   finalVideoChunk,
 } = require('../controller/downloadVideoChunks');
 
-const multer = require('multer');
-// const uploadVideo = require('../controller/uploadVideo');
-// const fileUploadConfig = require('../config/uploadFile');
-// const upload = multer(fileUploadConfig);
-
 const router = express.Router();
 
-// router.post('/save-video', upload.single('record'), uploadVideo);
 //videoUploda.start
 router.post('/start-record', startRecording);
 router.post('/video-chunks/:id', downloadVideo);
